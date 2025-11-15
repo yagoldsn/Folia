256fe1a065fe06f66732e0fe2bcaaf628d0ec2a3
yagoldsn <yagoldsn@gmail.com>
2025-11-15 16:44:51 -0300

    ajuste carrosel e textos seções

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,26 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const imagens = document.querySelector(".carrossel-imagens");
-  const total = imagens.children.length;
-  let index = 0;
+let count = 1;
 
-  const prevBtn = document.querySelector(".prev");
-  const nextBtn = document.querySelector(".next");
+document.getElementById("radio1").checked = true;
 
-  const larguraImagem = imagens.children[0].clientWidth;
-
-  nextBtn.addEventListener("click", () => {
-    index = (index + 1) % total;
-    imagens.style.transform = `translateX(-${index * larguraImagem}px)`;
-  });
-
-  prevBtn.addEventListener("click", () => {
-    index = (index - 1 + total) % total;
-    imagens.style.transform = `translateX(-${index * larguraImagem}px)`;
-  });
-
-  function atualizarCarrossel() {
-  const largura = document.querySelector(".carrossel").clientWidth;
-  imagens.style.transform = `translateX(-${index * largura}px)`;
+setInterval( function(){
+    nextImage();
+}, 5000)
+function nextImage(){
+    count++;
+    if(count > 4){
+        count = 1;
+    }
+    document.getElementById("radio" + count).checked = true;
 }
-
-});
